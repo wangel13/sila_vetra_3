9da0a4ab7a070b5516ce4517d423ac34c8befbe4
Taras Protchenko <tapacpro@gmail.com>
2022-11-01 21:25:39 +0300

    first one

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: "Taras",
+    email: "taras@taras.one",
+  },
+  {
+    name: "Vasiliy",
+    email: "vasiliy@taras.one",
+  },
+  {
+    name: "Alexey",
+    email: "alexey@taras.one",
+  },
+];
+
+async function main() {
+  console.log(`Start seeding ...`);
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    });
+    console.log(`Created user with id: ${user.id}`);
+  }
+  console.log(`Seeding finished.`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
