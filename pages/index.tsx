b0b2fb351950dae3b3298c59eaaa214b98a482d1
Taras Protchenko <tapacpro@gmail.com>
2022-11-06 20:13:36 +0300

    Add info icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,14 @@ import Layout from '../components/Layout'
 import useSWR from 'swr'
 import { useSession } from 'next-auth/react'
 
-type Props = {
-}
+type Props = {}
 
 export const fetcher = (url: string, ...args: any) => fetch(url, ...args).then((res) => res.json())
 
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
-});
+})
 
 const MainPage: React.FC<Props> = (props) => {
   const [tnved, setTnved] = useState('')
@@ -37,7 +36,7 @@ const MainPage: React.FC<Props> = (props) => {
       <div className="page">
         <main className="container mx-auto mb-10">
           <h1 className="text-2xl font-bold mb-8">Рейтинг ТНВЭД для замещения:</h1>
-          <div className="pb-8 flex gap-4">
+          <div className="pb-8 flex gap-4 items-center">
             <input
               value={tnved}
               className="py-2 px-4 border rounded"
@@ -58,6 +57,22 @@ const MainPage: React.FC<Props> = (props) => {
               <option>2021</option>
               <option>2022</option>
             </select>
+            <a title="выводятся первые 200 ТН ВЭДов">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-gray-400 hover:text-gray-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                />
+              </svg>
+            </a>
           </div>
           <div className="flex gap-4 flex-col">
             <div className="shadow bg-white rounded p-4 flex gap-8 flex-nowrap font-bold text-gray-600">
@@ -72,8 +87,12 @@ const MainPage: React.FC<Props> = (props) => {
                 <div key={tnved?.tnved} className="shadow bg-white rounded p-4 flex gap-8 flex-nowrap">
                   <div className="basis-1/12">{tnved?.tnved}</div>
                   <div className="basis-6/12">{tnved?.tnved_title}</div>
-                  <div className="basis-2/12">{tnved?.import_amount_usd ? formatter.format(tnved?.import_amount_usd) : 0}</div>
-                  <div className="basis-2/12">{tnved?.export_amount_usd ? formatter.format(tnved?.export_amount_usd) : 0}</div>
+                  <div className="basis-2/12">
+                    {tnved?.import_amount_usd ? formatter.format(tnved?.import_amount_usd) : 0}
+                  </div>
+                  <div className="basis-2/12">
+                    {tnved?.export_amount_usd ? formatter.format(tnved?.export_amount_usd) : 0}
+                  </div>
                   <div className="basis-1/12">{tnved?.score}</div>
                 </div>
               ))}
